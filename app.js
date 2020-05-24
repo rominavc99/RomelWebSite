7eb76be0a932e674fa69905134178d84f9f43c55
rominavc99 <procrom1999@gmail.com>
2020-05-23 19:31:59 -0700

    update1 no responsive

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ $(function() {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+!function($_x,_s,id){
+  var js,fjs=$_x.getElementsByTagName(_s)[0];
+  if(!$_x.getElementById(id))
+    {
+    js = $_x.createElement(_s);
+    js.id = id;js.src = "//platform.docplanner.com/js/widget.js";
+    fjs.parentNode.insertBefore(js,fjs);
+    }
+}
+(document,"script","zl-widget-s");
